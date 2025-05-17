00efde9d45d93e05d702b301f0973cc3f8550c68
dnsxmrs <ericemarial@gmail.com>
2025-05-17 20:51:53 +0800

    chore: update error handling in TestErrorPage for testing purposes
    - Commented out the error throw statement to allow for testing without triggering an error.
    - Added comments to guide future testing and production usage.

--- src/app/test/error/page.tsx
+++ src/app/test/error/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 export default function TestErrorPage() {
-    throw new Error("This is a test error. (app/test/error/page.tsx)");
+    // uncomment this to test error
+    // comment this error for production
+    // throw new Error("This is a test error. (app/test/error/page.tsx)");
     return <div>Home</div>;
 }
