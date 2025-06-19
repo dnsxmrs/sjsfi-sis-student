2f1ea33412826203925b61b7a0551d004f5b9412
BenJr23 <rubenjrtbertuso@gmail.com>
2025-06-19 21:24:59 +0800

    Working fetch for the active students

--- src/app/registrar/home/page.tsx
+++ src/app/registrar/home/page.tsx
@@ -1,18 +1,19 @@
 import StatsCards from '@/components/registrar/StatsCards';
 import { RecentRegistrationsTable } from '@/components/registrar/RecentRegistrationTable';
 import { RecentPendingRegistrations } from '@/components/registrar/RecentPendingRegistration';
+import { getActiveStudentCount } from '@/app/_actions/getDashboardData';
 
-export default function RegistrarHomePage() {
+export default async function RegistrarHomePage() {
+  const activeStudentCount = await getActiveStudentCount();
   return (
     <div className="p-6 space-y-6">
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-            <StatsCards />
+            <StatsCards activeStudentCount={activeStudentCount} />
         </div>
         <div className="grid md:grid-cols-2 gap-4">
             <RecentRegistrationsTable />
             <RecentPendingRegistrations />
         </div>
     </div>
-   
   );
 }
