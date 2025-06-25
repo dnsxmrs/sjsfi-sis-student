1f7ffa963f20e50c57d60310156a5702faaa3133
BenJr23 <rubenjrtbertuso@gmail.com>
2025-06-26 02:51:58 +0800

    feat: change form birthday selection to actual dates

--- src/app/(auth)/registrar/register-student/Form-students/fatherbackground.tsx
+++ src/app/(auth)/registrar/register-student/Form-students/fatherbackground.tsx
@@ -63,12 +63,7 @@ export default function FatherBackgroundPage({ onBack }: FatherBackgroundPagePro
         <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
           <div>
             <label className="block text-sm font-medium mb-1 text-black">Birth Date:</label>
-            <div className="relative">
-              <input type="text" placeholder="MM/DD/YY" className="input input-bordered w-full pr-8 text-black bg-gray-100 border border-gray-300" />
-              <span className="absolute right-2 top-2 text-gray-400">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" /></svg>
-              </span>
-            </div>
+            <input type="date" className="input input-bordered w-full text-black bg-gray-100 border border-gray-300" />
           </div>
           <div>
             <label className="block text-sm font-medium mb-1 text-black">Place of Birth:</label>
